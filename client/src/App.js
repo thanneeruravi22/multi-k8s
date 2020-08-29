20dc2fd5ca78f3adc76c33e2dede7b844689fb24
thanneeruravi22 <thanneeruravi22@gmail.com>
2020-08-29 10:48:04 +0000

    Client from check point

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
-import OtherPage from './otherpage';
+import OtherPage from './OtherPage';
 import Fib from './Fib';
 
 class App extends Component {
